41a9518bc01f9b4bb2ef828d8bf131e9a3a3adad
zgorzal <zgorzalek.piotr@gmail.com>
2021-07-12 12:37:28 +0200

    SecurityConfig add

---
+++ src/main/java/pl/zgorzalek/web_spy/app/SecurityConfig.java
@@ -0,0 +1,26 @@
+package pl.zgorzalek.web_spy.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}user").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}admin").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").authenticated()
+                .and().formLogin();
+    }
+}
