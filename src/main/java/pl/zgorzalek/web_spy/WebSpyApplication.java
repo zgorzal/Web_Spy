2d24071e551f014db955b1043517a85ad8ca9ea3
zgorzal <zgorzalek.piotr@gmail.com>
2021-07-12 11:27:34 +0200

    project init

---
+++ src/main/java/pl/zgorzalek/web_spy/WebSpyApplication.java
@@ -0,0 +1,13 @@
+package pl.zgorzalek.web_spy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSpyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/zgorzalek/web_spy/WebSpyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zgorzalek.web_spy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
