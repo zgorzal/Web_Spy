871b8516db7e453c9530ed371cb7d4d346b5f1e0
zgorzal <zgorzalek.piotr@gmail.com>
2021-07-13 12:55:12 +0200

    add HomeController

---
+++ src/main/java/pl/zgorzalek/web_spy/HomeController.java
@@ -0,0 +1,15 @@
+package pl.zgorzalek.web_spy;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    @RequestMapping("/")
+    public String loginOn() {
+        return "dashboard";
+    }
+}
--- src/main/java/pl/zgorzalek/web_spy/LoginController.java
+++ src/main/java/pl/zgorzalek/web_spy/LoginController.java
@@ -3,7 +3,6 @@ package pl.zgorzalek.web_spy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,10 +11,5 @@ public class LoginController {
     @GetMapping("/login")
     public String login() {
         return "login";
-    }
-
-    @RequestMapping("/")
-    public String loginOn() {
-        return "dashboard";
     }
 }
