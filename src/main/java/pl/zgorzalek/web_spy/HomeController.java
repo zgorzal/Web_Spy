af7c34da2f7951dbad7f754920bd73a7eee87786
zgorzal <zgorzalek.piotr@gmail.com>
2021-07-14 15:54:17 +0200

    add first name i last name in dashboard

--- src/main/java/pl/zgorzalek/web_spy/HomeController.java
+++ src/main/java/pl/zgorzalek/web_spy/HomeController.java
@@ -1,15 +1,28 @@
 package pl.zgorzalek.web_spy;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.zgorzalek.web_spy.user.User;
+import pl.zgorzalek.web_spy.user.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+    private final UserService userService;
 
     @RequestMapping("/")
-    public String loginOn() {
+    public String loginOn(HttpServletRequest request) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = auth.getName();
+        User user = userService.findByEmail(email);
+        request.setAttribute("firstName", user.getFirstName());
+        request.setAttribute("lastName", user.getLastName());
         return "dashboard";
     }
 }
